f15d267804676c5d26d34f8037059f43458676b4
Dewa Bisma <idewagedebisma@gmail.com>
2021-02-26 14:59:43 +0800

    finish dashboard and header

--- frontend/src/screens/DashboardScreen/DashboardScreen.jsx
+++ frontend/src/screens/DashboardScreen/DashboardScreen.jsx
@@ -1,45 +1,74 @@
 import React from 'react';
-import { Row, Col, Table, ListGroup } from 'react-bootstrap';
+import { Row, Col, Table, ListGroup, Card } from 'react-bootstrap';
 
 import Header from '../../components/Header/Header';
 
+import styles from './DashboardScreen.module.scss';
+
 const DashboardScreen = () => {
+  window.onresize = () => {};
+
   return (
     <>
       <Header />
 
       <Row noGutters>
         <Col className='bg-light pt-5' md={3}>
-          <ListGroup defaultActiveKey='#link1' variant='flush'>
-            <ListGroup.Item action href='#link1'>
+          <ListGroup variant='flush'>
+            <ListGroup.Item variant='primary' action>
               Link 1
             </ListGroup.Item>
-            <ListGroup.Item action href='#link2' disabled>
+            <ListGroup.Item variant='primary' action>
               Link 2
             </ListGroup.Item>
-            <ListGroup.Item action>This one is a button</ListGroup.Item>
+            <ListGroup.Item variant='primary' action>
+              This one is a button
+            </ListGroup.Item>
           </ListGroup>
         </Col>
 
-        <Col md={9}>
-          <Row className='p-4' noGutters>
-            <Col xs={12} sm={3}>
-              kotak
+        <Col className='p-4' md={9}>
+          <Row noGutters>
+            <Col xs={12} sm={6} md={3}>
+              <Card className='bg-light border-0 shadow mr-sm-2 mb-sm-2 mr-md-0'>
+                <Card.Body>
+                  <Card.Title>Nota Terinput</Card.Title>
+                  <Card.Text>240</Card.Text>
+                </Card.Body>
+              </Card>
             </Col>
-            <Col xs={12} sm={3}>
-              kotak
+
+            <Col xs={12} sm={6} md={3}>
+              <Card className='bg-light border-0 shadow mt-2 mt-sm-0 ml-sm-2 mb-sm-2 m-md-0 ml-md-2'>
+                <Card.Body>
+                  <Card.Title>Nota Terinput</Card.Title>
+                  <Card.Text>240</Card.Text>
+                </Card.Body>
+              </Card>
             </Col>
-            <Col xs={12} sm={3}>
-              kotak
+
+            <Col xs={12} sm={6} md={3}>
+              <Card className='bg-light border-0 shadow mt-2 mt-sm-2 mr-sm-2 m-md-0 ml-md-2'>
+                <Card.Body>
+                  <Card.Title>Nota Terinput</Card.Title>
+                  <Card.Text>240</Card.Text>
+                </Card.Body>
+              </Card>
             </Col>
-            <Col xs={12} sm={3}>
-              kotak
+
+            <Col xs={12} sm={6} md={3}>
+              <Card className='bg-light border-0 shadow mt-2 ml-sm-2 mt-sm-2 m-md-0 ml-md-2'>
+                <Card.Body>
+                  <Card.Title>Nota Terinput</Card.Title>
+                  <Card.Text>240</Card.Text>
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
 
-          <Row className='p-4' noGutters>
-            <Col>
-              <Table responsive='xs'>
+          <Row className='pt-4' noGutters>
+            <Col className='p-2 bg-light shadow '>
+              <Table responsive borderless>
                 <thead>
                   <tr>
                     <th>#</th>
@@ -52,7 +81,7 @@ const DashboardScreen = () => {
                   </tr>
                 </thead>
 
-                <tbody>
+                <tbody className={styles.table}>
                   <tr>
                     <td>1</td>
                     <td>Table cell</td>
@@ -82,14 +111,64 @@ const DashboardScreen = () => {
                     <td>Table cell</td>
                     <td>Table cell</td>
                   </tr>
+
+                  <tr>
+                    <td>4</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+
+                  <tr>
+                    <td>5</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+
+                  <tr>
+                    <td>6</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+
+                  <tr>
+                    <td>7</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+
+                  <tr>
+                    <td>8</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
                 </tbody>
               </Table>
             </Col>
           </Row>
 
-          <Row className='p-4' noGutters>
-            <Col>
-              <Table responsive='xs'>
+          <Row className='pt-4' noGutters>
+            <Col className='p-2 bg-light shadow '>
+              <Table responsive borderless>
                 <thead>
                   <tr>
                     <th>#</th>
