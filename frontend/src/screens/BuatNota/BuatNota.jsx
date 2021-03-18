65533a3323e8308d1c9cfa3432887cb9927ef7b1
Dewa Bisma <idewagedebisma@gmail.com>
2021-03-18 16:20:33 +0800

    add state of form, finish form

--- frontend/src/screens/BuatNota/BuatNota.jsx
+++ frontend/src/screens/BuatNota/BuatNota.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Row, Col, Button, Table } from 'react-bootstrap';
+import { Row, Col, Button, Form, Table } from 'react-bootstrap';
+import useFetch from '../../hooks/useFetch.js';
 
 import Header from '../../components/Header/Header';
 import Loader from '../../components/Loader/Loader';
@@ -11,6 +12,30 @@ import SideMenu from '../../components/SideMenu/SideMenu';
 
 const BuatNota = ({ history }) => {
   const dispatch = useDispatch();
+
+  const {
+    data: listBiaya,
+    error: errorListBiaya,
+    isLoading: loadingListBiaya,
+  } = useFetch('/api/prices');
+
+  // Form 1 - Rincian Pengiriman
+  const [namaPengirim, setNamaPengirim] = useState('');
+  const [namaPenerima, setNamaPenerima] = useState('');
+  const [alamatPenerima, setAlamatPenerima] = useState('');
+  const [dataBarang, setDataBarang] = useState([]);
+  const [totalColli, setTotalColli] = useState(0);
+  const [totalBerat, setTotalBerat] = useState(0);
+  const [totalHarga, setTotalHarga] = useState(0);
+
+  // Form 2 - Data Barang Dikirim
+  const [banyakColli, setBanyakColli] = useState(0);
+  const [macamColli, setMacamColli] = useState('');
+  const [merekColli, setMerekColli] = useState('');
+  const [namaBarang, setNamaBarang] = useState('');
+  const [beratKotor, setBeratKotor] = useState(0);
+  const [biayaAngkut, setBiayaAngkut] = useState(0);
+  const [keterangan, setKeterangan] = useState('');
 
   useEffect(() => {
     if (false) {
@@ -27,7 +52,93 @@ const BuatNota = ({ history }) => {
           <SideMenu history={history} page='buatNota' />
         </Col>
 
-        <Col className='p-4' md={9}></Col>
+        <Col className='p-4' md={9}>
+          <h1 className=''>Input Nota</h1>
+
+          <Row noGutters>
+            <Col>
+              <Form>
+                <Form.Group controlId='namaPengirim'>
+                  <Form.Label>Nama Pengirim</Form.Label>
+                  <Form.Control
+                    type='text'
+                    placeholder='Masukkan nama pengirim'
+                    value={namaPenerima}
+                    onChange={(e) => setNamaPenerima(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group controlId='namaPenerima'>
+                  <Form.Label>Nama Penerima</Form.Label>
+                  <Form.Control
+                    type='text'
+                    placeholder='Masukkan nama penerima'
+                    required
+                  />
+                </Form.Group>
+
+                <Form.Group controlId='alamatPenerima'>
+                  <Form.Label>Alamat Penerima</Form.Label>
+                  <Form.Control
+                    type='text'
+                    placeholder='Masukkan alamat penerima'
+                    required
+                  />
+                </Form.Group>
+
+                <Button variant='primary' type='submit'>
+                  Buat Nota
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+
+          <Row className='mt-3' noGutters>
+            <Col>
+              <h2>Data Barang</h2>
+
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Banyak Colli</th>
+                    <th>Macam Colli</th>
+                    <th>Merek Colli</th>
+                    <th>Nama Barang</th>
+                    <th>Berat Kotor</th>
+                    <th>Biaya Angkut</th>
+                    <th>Keterangan</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>1234</td>
+                    <td>1234</td>
+                    <td>1234</td>
+                    <td>1234</td>
+                    <td>1234</td>
+                    <td>1234</td>
+                    <td>1234</td>
+                  </tr>
+                  {dataBarang.map((barang) => (
+                    <tr>
+                      <td>{barang.noBarang}</td>
+                      <td>{barang.banyakColli}</td>
+                      <td>{barang.macamColli}</td>
+                      <td>{barang.merekColli}</td>
+                      <td>{barang.namaBarang}</td>
+                      <td>{barang.beratKotor}</td>
+                      <td>{barang.biayaAngkut}</td>
+                      <td>{barang.keterangan}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Col>
       </Row>
     </>
   );
